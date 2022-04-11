3c0255d1f19bbeac9c93020a966c1718743a8ebd
WMoth <dldngud97@naver.com>
2022-04-11 15:12:45 +0900

    Create 1927.cpp

---
+++ 1927.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> Q;
+int n, k;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    while(n--)
+    {
+        cin >> k;
+        if(k > 0)
+            Q.push(k);
+        else
+        {
+            if(Q.empty())
+                cout << 0 << "\n";
+            else
+            {
+                cout << Q.top() << "\n";
+                Q.pop();
+            }
+        }
+    }
+}
