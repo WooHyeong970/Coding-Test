2c81a0468c7730c8da8ab503dafa4b95c09bbf43
WMoth <dldngud97@naver.com>
2022-04-26 16:36:58 +0900

    Create 11719.cpp

---
+++ 11719.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    while(getline(cin, s))
+        cout << s << "\n";
+}
